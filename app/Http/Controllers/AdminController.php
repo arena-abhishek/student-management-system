cad8cdc38daff03250a6410345c85a5e71698eba
arena-abhishek <apdesign0001@gmail.com>
2025-01-09 14:43:33 +0530

    add

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,49 +2,61 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Auth;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AdminController extends Controller
 {
-    public function index()
-    {
-        return view('admin.login');
+  public function index()
+  {
+    return view('admin.login');
+  }
+  public function authenticate(Request $req)
+  {
+    $req->validate([
+      'email' => 'required|email',
+      'password' => 'required'
+    ]);
+    if (Auth::guard('admin')->attempt(['email' => $req->email, 'password' => $req->password])) {
+      if (Auth::guard('admin')->user()->role != 'admin') {
+        Auth::guard('admin')->logout();
+        return redirect()->route('admin.login')->with('error', 'Unauthorized user Access denied');
+      }
+      return redirect()->route('admin.dashboard');
+    } else {
+      return redirect()->route('admin.login')->with('error', 'Something went wrong');
     }
-    public function authenticate(Request $req)
-    {
-        $req->validate([
-            'email' => 'required|email',
-            'password' => 'required'
-        ]);
-        if (Auth::guard('admin')->attempt(['email' => $req->email, 'password' => $req->password])) {
-            echo "ok";
-        } else {
-            return redirect()->route('admin.login')->with('error', 'Something went wrong');
-        }
-    }
-    public function register()
-    {
-        $user = new User();
-        $user->name = "admin";
-        $user->role = "admin";
-        $user->email = "admin@gmail.com";
-        $user->password = Hash::make('admin');
-        return redirect()->route('admin.login')->with('success', 'User created succesfully');
+  }
+  public function register()
+  {
+    $user = new User();
+    $user->name = "admin";
+    $user->role = "admin";
+    $user->email = "admin@gmail.com";
+    $user->password = Hash::make('admin');
+    $user->save();
+    return redirect()->route('admin.login')->with('success', 'User created succesfully');
 
-    }
-    public function dashboard()
-    {
-        return view('admin.dashboard');
-    }
-    public function form()
-    {
-        return view('admin.form');
-    }
-    public function table()
-    {
-        return view('admin.table');
-    }
+  }
+  public function dashboard()
+  {
+    return view('admin.dashboard');
+  }
+  public function logout()
+  {
+    Auth::guard('admin')->logout();
+    return redirect()->route('admin.login')->with('success', 'Logout successfully');
+  }
+  public function form()
+  {
+    return view('admin.form');
+  }
+  public function table()
+  {
+    return view('admin.table');
+  }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,25 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('admin/login', [AdminController::class, 'index'])->name('admin.login');
+Route::group(['prefix' => 'admin'], function () {
+    Route::group(['middleware' => 'admin.guest'], function () {
+        Route::get('login', [AdminController::class, 'index'])->name('admin.login');
 
-Route::get('admin/registered', [AdminController::class, 'register'])->name('admin.register');
+        Route::get('registered', [AdminController::class, 'register'])->name('admin.register');
 
-Route::post('admin/login', [AdminController::class, 'authenticate'])->name('admin.authenticate');
+        Route::post('login', [AdminController::class, 'authenticate'])->name('admin.authenticate');
 
-Route::get('admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+    });
+    Route::group(['middleware' => 'admin.auth'], function () {
+        Route::get('logout', [AdminController::class, 'logout'])->name('admin.logout');
 
-Route::get('admin/form', [AdminController::class, 'form'])->name('admin.form');
+        Route::get('dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
 
-Route::get('admin/table', [AdminController::class, 'table'])->name('admin.form');
+        Route::get('form', [AdminController::class, 'form'])->name('admin.form');
+
+        Route::get('table', [AdminController::class, 'table'])->name('admin.table');
+
+    });
+});
+
+
